08d560dbe9705cec5b7cf014cc7f369f51068360
JoaoMoraesJr <joaojr1997@gmail.com>
2021-02-21 21:23:33 -0300

    Add project card information

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -4,10 +4,16 @@ import thumbnail from '../../assets/img/project-images/default_code0.jpg';
 function ProjectCard() {
     return (
         <div>
-            <div>
-                <div className="gradient-background">
-                    <img src={thumbnail} className="thumbnail" alt="thumbnail"/>
-                    <p className="project-name">Project Name</p>
+            <div className="gradient-background">
+                <img src={thumbnail} className="thumbnail" alt="thumbnail"/>
+                <p className="card-name card-text">Project Name</p>
+                <div className="card-info card-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas accumsan placerat purus sit amet pellentesque.</p>
+                    <p>Year: 2020</p>
+                    <div className= "tag-list">
+                        <div className="tag">AI</div>
+                        <div className="tag">Research</div>
+                    </div>
                 </div>
             </div>
         </div>
