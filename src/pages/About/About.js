0bcc4c8eff57dab996f2c7d9ad44d57b2d74d293
JoaoMoraesJr <joaojr1997@gmail.com>
2021-04-18 21:46:33 -0300

    Start about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,12 +1,19 @@
 import './About.scss';
 import { FaLinkedin, FaGithub, FaItchIo, FaInstagram } from 'react-icons/fa'
 import { GrMail } from "react-icons/gr";
+import {ABOUT_DATA} from "../../assets/data/AboutData";
 
 function About() {
 
+    let about = ABOUT_DATA;
+
     return(
         <div>
-            <span>About page works!</span>
+            <div className="about-headline">
+                <div><img src={about.profilePhoto} className="profile-photo" alt="profile-photo"/></div>
+                <h1 className="about-title">{about?.title}</h1>
+                <p className="about-text">{about?.description}</p>
+            </div>
         </div>
     )
 }
