aa78f25c9e9d67aec329b1456aff754340361e6b
JoaoMoraesJr <joaojr1997@gmail.com>
2021-04-18 21:25:46 -0300

    Home page

---
+++ src/assets/data/HomeData.js
@@ -0,0 +1,7 @@
+export const HOME_DATA = {
+    title: "About me",
+    headline: "A Creative Computer Scientist ready to bring Inovation",
+    profilePhoto: require("../img/profile_photo.png").default,
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sit amet volutpat nisl, et finibus sem. Nullam auctor lectus a dolor ornare tempus. Suspendisse volutpat neque ut velit tincidunt, sed rhoncus leo hendrerit. Aliquam condimentum, diam nec placerat interdum, nunc tortor finibus urna, ac cursus eros risus nec lacus. Aenean lacinia varius efficitur. Praesent nibh leo, hendrerit sagittis elit nec, luctus consequat nunc. Nulla id sem eros. Fusce tincidunt non arcu quis vulputate. Aenean orci nisi, sollicitudin vestibulum ligula id, egestas facilisis mi. Duis interdum, mauris vitae aliquet efficitur, nibh velit scelerisque augue, ac commodo nunc metus vitae elit. Vivamus a elit magna. Etiam posuere vehicula ipsum, sed tempor purus ullamcorper sed. Praesent nec massa quis metus auctor tempus.",
+    
+}
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,45 @@
 import './Home.scss';
-import { FaLinkedin, FaGithub, FaItchIo, FaInstagram } from 'react-icons/fa'
+import { FaAngular, FaReact, FaUnity, FaPython, FaGithub, FaItchIo, FaInstagram } from 'react-icons/fa'
+import { SiDotNet, SiBlender } from 'react-icons/si'
 import { GrMail } from "react-icons/gr";
-
+import {HOME_DATA} from "../../assets/data/HomeData";
 function Home() {
 
+    let home = HOME_DATA;
+
     return(
-        <div>
-            <span>Home page works!</span>
+        <div className="home">
+            <div className="home-header">
+                <div><img src={home.profilePhoto} className="profile-photo" alt="profile-photo"/></div>
+                <h1 className="headline">{home?.headline}</h1>
+                <p className="home-text">{home?.description}</p>
+            </div>
+            <div className="technologies">
+                <span><strong>What I do</strong></span>
+                <p className="home-text">Subtitle about my work experience and what I expect the users to feel when navigating this website</p>
+                <ul className="technologies-list">
+                    <li className = "technologies-item">
+                        <div className="technologies-icons"><FaAngular /><FaReact /><SiDotNet /></div>
+                        <span><strong>Web development</strong></span>
+                        <span>Full stack experience in web development</span>
+                    </li>
+                    <li className = "technologies-item">
+                        <div className="technologies-icons"><FaUnity /><SiBlender /></div>
+                        <span><strong>Game Development</strong></span>
+                        <span>experience building games from art to coding</span>
+                    </li>
+                    <li className = "technologies-item" style={{paddingRight: 0}}>
+                        <div className="technologies-icons"><FaPython /></div>
+                        <span><strong>Artificial Inteligence</strong></span>
+                        <span>Experience on research, developing AI models</span>
+                    </li>
+                </ul>
+                <div className="center-item">
+                <a className="button-primary" href="/about">
+                    <span>See more about me</span>
+                </a>
+                </div>
+            </div>
         </div>
     )
 }
