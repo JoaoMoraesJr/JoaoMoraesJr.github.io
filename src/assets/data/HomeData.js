0039cd2362f6acc0b54470529cae74c09b96147e
JoaoMoraesJr <joaojr1997@gmail.com>
2022-04-21 21:07:47 -0300

    Fix typo and profile photo filter

--- src/assets/data/HomeData.js
+++ src/assets/data/HomeData.js
@@ -1,5 +1,5 @@
 export const HOME_DATA = {
-    headline: "A Creative Computer Scientist ready to bring Inovation",
+    headline: "A Creative Computer Scientist ready to bring Innovation",
     profilePhoto: require("../img/profile_photo.png").default,
     description: "Hello, my name is João Moraes, a <b>Computer Scientist</b> currently working at DELL as a <b>Software Engineer</b>. I love everything related to technology, and particularly in some areas that I like to focus like web development, game development, and artificial intelligence.<br><br>With a <b>curious mind</b> that needs to be constantly fed with more knowledge and discoveries, I have a special <b>passion for creating</b> things, that extends to many different areas, like applications, robots, games, arts, music, etc. This website is my space to showcase some of these interests, be welcomed!",
 }
